9890c46a52abb54961223ab8ce1b83a6f1d7ca0a
murthehelp24 <attachaijumpahom@gmail.com>
2026-03-11 12:30:18 +0700

    notFound + Error middleware

--- API/src/app.js
+++ API/src/app.js
@@ -2,6 +2,8 @@ import express from 'express'
 import authRoute from './routes/auth.routes.js'
 import postRoute from './routes/post.routes.js'
 import commentRoute from './routes/comment.routes.js'
+import notFoundMiddleware from './middlewares/notFound.middleware.js'
+import errorMiddleware from './middlewares/error.middleware.js'
 
 const app = express()
 app.use(express.json())
@@ -14,8 +16,14 @@ app.use('/api/post', postRoute)
 
 app.use('/api/comment', commentRoute)
 
+
+// ยังไม่ได้เขียน router, controller
 app.use('/api/like', (req, res) => {
   res.send('auth like')
 })
+// not found
+app.use(notFoundMiddleware)
+// error middleware
+app.use(errorMiddleware)
 
 export default app
---
+++ API/src/middlewares/error.middleware.js
@@ -0,0 +1,8 @@
+
+
+export default function(err, req, res, next){ 
+  console.error(err)
+  res.status(500).json({
+    message : err.message
+  }) 
+}
